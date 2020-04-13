50a3e36a5c5e3303000ef30a7e039dbfabf432cd
Arthurprnn <arthur.peresson@unvi-tlse3.fr>
2020-04-13 16:17:12 +0200

    V-31

---
+++ include/html.h
@@ -0,0 +1,12 @@
+#ifndef __HTML_H__
+#define __HTML_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "objet.h"
+#include "users.h"
+
+
+void AfficherObjetsParType(char*);
+void afficheObjetsPersonne(Personne);
+#endif
